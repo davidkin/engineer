cad8a79a1421a2d606b886a7f84b12a11445800b
David <davidfloyd322@gmail.com>
2019-04-11 16:43:29 +0300

    feature/modal, add modal template

---
+++ src/app/home/modal/modal.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { IPosts } from 'src/app/shared/interface/posts.interface';
+import { HomeService } from '../home.services';
+import { Observable } from 'rxjs';
+
+@Component({
+    selector: 'modal',
+    templateUrl: './modal.component.html',
+    styleUrls: ['./modal.component.css']
+})
+export class ModalComponent implements OnInit {
+    post: Observable<IPosts>;
+
+    constructor(
+        private homeService: HomeService
+    ) { }
+
+    ngOnInit(): void { }
+
+    ngDoCheck(): void {
+       this.post = this.homeService.getPosts();
+    }
+}
